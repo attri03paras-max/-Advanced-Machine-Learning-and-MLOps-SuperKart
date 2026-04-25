32487d9a38084db3223959cfeb6eeb5254d88af1
attri03paras-max <attri03paras@gmail.com>
2026-04-25 17:57:57 +0530

    Add data preprocessing script for CSV files

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import os
 
+df = pd.read_csv("data/superkart_data.csv")
+
+df.dropna(inplace=True)
+df = pd.get_dummies(df)
+
+os.makedirs("data/processed", exist_ok=True)
+df.to_csv("data/processed/processed_data.csv", index=False)
+
+print("Preprocessing done")
